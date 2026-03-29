1e99ae39e794528a89254a2b8428a8d92c481627
ray <230245401+bigtechray29@users.noreply.github.com>
2026-03-29 12:56:08 -0400

    Add files via upload

---
+++ 00-auth.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0" # Use latest version if possible
+    }
+  }
+
+  backend "s3" {
+    bucket  = "jenkins-3"          # Name of the S3 bucket
+    key     = "jenkins-test-032226.tfstate" # The name of the state file in the bucket
+    region  = "us-west-1"                   # Use a variable for the region
+    encrypt = true                          # Enable server-side encryption (optional but recommended)
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
